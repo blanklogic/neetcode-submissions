87d603cdccef4e1dd46db38d4d4da8a280fee33c
Jaymeson Koh <73272798+blanklogic@users.noreply.github.com>
2026-08-28 14:44:55 -0400

    Bulk sync: 32 submissions

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-0.py
@@ -0,0 +1,13 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        res = [1] * (len(nums))
+
+        pref = 1
+        for i in range(len(nums)):
+            res[i] = pref
+            pref *= nums[i]
+        pfix = 1
+        for i in range(len(nums) - 1, -1, -1):
+            res[i] *= pfix
+            pfix *= nums[i]
+        return res
